0066ab685d9b23551b6b924b9f111b270e338cb2
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-10-11 14:05:44 -0700

    fix root to root to in config/routes'

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/store';
+// import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  // const store = configureStore();
 
   //testing
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
   //testing
 
   const root = document.getElementById('root');
