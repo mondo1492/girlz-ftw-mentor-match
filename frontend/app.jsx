9b82dd4edf912cf6b4e75203ad6f024bb708123c
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-10-11 14:45:01 -0700

    create util folder with session api calls and create reducers and store folders"

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import configureStore from './store/store';
+import configureStore from './store/store';
+import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const store = configureStore();
+  const store = configureStore();
 
   //testing
-  // window.getState = store.getState;
-  // window.dispatch = store.dispatch;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
   //testing
 
   const root = document.getElementById('root');
