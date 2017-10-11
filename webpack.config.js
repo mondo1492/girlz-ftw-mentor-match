357bd38af8060c98c11ef0a925efcc96ac816e47
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-10-11 13:32:24 -0700

    change entry file name to app and update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ var path = require("path");
 // include plugins config
 module.exports = {
   context: __dirname,
-  entry: "./frontend/index",
+  entry: "./frontend/app",
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
