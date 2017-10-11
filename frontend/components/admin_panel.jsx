b5b48cbe6f085aa0972d76f76ce155e0213ad084
Ravi <raviraval@gmail.com>
2017-10-11 16:32:51 -0700

    work on admin panel

---
+++ frontend/components/admin_panel.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AdminPanel = () => (
+  <div>
+    <header>
+      <h1>Admin Panel</h1>
+    </header>
+    <p>adminy stuff</p>
+
+    <button>Launch Applications</button>
+
+    <button>Generate Matches</button>
+
+  </div>
+);
+
+export default AdminPanel;
