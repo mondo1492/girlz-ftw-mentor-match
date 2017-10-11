2878d2961cda8b2eacc3ca5f978cb85a98c0d24f
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-10-11 13:49:12 -0700

    start frontend

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,1 +1,15 @@
-//hello
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  //testing
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  //testing
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Welcome to GirlZftw Mentorship</h1>, root);
+});
