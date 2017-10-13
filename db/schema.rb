8ce7fbf0469db26decb680f8c9a27c3bb5129f9d
Ravi <raviraval@gmail.com>
2017-10-13 12:50:46 -0700

    update db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011212746) do
+ActiveRecord::Schema.define(version: 20171013193833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,32 @@ ActiveRecord::Schema.define(version: 20171011212746) do
   create_table "mentees", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "approved", default: false, null: false
+    t.boolean "status", default: false, null: false
+    t.integer "age", null: false
+    t.string "city", null: false
+    t.string "country", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "phone"
+    t.string "facebook", null: false
+    t.string "email", null: false
+    t.string "high_school"
+    t.string "college"
+    t.string "major"
+    t.string "employer"
+    t.integer "career_advice_rank", null: false
+    t.integer "personal_advice_rank", null: false
+    t.integer "motivation_rank", null: false
+    t.text "instagram_bio_text"
+    t.text "instagram_bio_why_not_text"
+    t.integer "share_major_rank"
+    t.text "personality_text"
+    t.text "night_text"
+    t.text "not_on_google_text"
+    t.text "how_impact_text"
+    t.text "no_discuss_text"
+    t.text "extra_info_text"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,6 +53,28 @@ ActiveRecord::Schema.define(version: 20171011212746) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
+    t.boolean "approved", default: false, null: false
+    t.boolean "status", default: false, null: false
+    t.integer "age", null: false
+    t.string "city", null: false
+    t.string "country", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "phone"
+    t.string "picture_url"
+    t.string "facebook", null: false
+    t.string "email", null: false
+    t.string "high_school"
+    t.string "college"
+    t.string "major"
+    t.string "employer"
+    t.integer "career_advice_rank", null: false
+    t.integer "personal_advice_rank", null: false
+    t.integer "motivation_rank", null: false
+    t.integer "share_major_rank"
+    t.text "personality_text"
+    t.text "why_mentor_text"
+    t.text "extra_info_text"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
