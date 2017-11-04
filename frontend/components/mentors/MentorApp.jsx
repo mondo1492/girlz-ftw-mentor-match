9d163001535a41f106791ebc336485a544350d83
Ravi <raviraval@gmail.com>
2017-11-03 18:23:56 -0700

    prep mentor form with bootstrap

--- frontend/components/mentors/MentorApp.jsx
+++ frontend/components/mentors/MentorApp.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Panel } from 'react-bootstrap';
+import { Button, Panel, FormControl, ControlLabel, FormGroup, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class MentorApp extends React.Component {
   constructor(props) {
@@ -64,64 +64,91 @@ class MentorApp extends React.Component {
     return (
       <div>
 
+        <Panel collapsible expanded={this.state.disclaimer}>
+
+          <p>
+            Hello! Thank you for your interest in the GIRLZ, FTW mentorship program. We’re thrilled to read your application. Before you fill out our form, we’d like to let you in on a little secret. We’re not a regular mentorship program. We’re a community of girlz that are passionate about elevating and celebrating female voices around the world. We don’t expect our mentors to be THAT girl… you know, the one that always had her shit together and still always managed to blow dry her hair before school / work. We’re looking for real, passionate, #girlbosses that don’t pretend to be perfect, can hustle their way to their dreams, AND still manage to uplift others along the way.
+          </p>
+
+          <p>
+            What you should know before submitting your application:
+          </p>
+          <ListGroup>
+            <ListGroupItem>
+              All of our communication once you join the program happens on Facebook. If you’re accepted as a mentor, you will be added to a Facebook group. This group will be the place where you can connect with other mentors and mentees in the program. We’ll use this group to provide support, encouragement, and advice for each other. You must have a Facebook page if you want to be a part of this program.
+            </ListGroupItem>
+            <ListGroupItem>
+              Every month, we’ll host a call with all mentors and mentees. These calls take place on the first friday of every month. In these calls, we’ll have inspiring women talk to about how they got to where they are today. Each call will be structured differently but is meant to inspire, inform, and mobilize everyone on the call. This means you’ll probably be tapped to speak at one of these calls because we suspect you’re awesome! Attending these calls is mandatory. You can only miss up to 3 calls.
+            </ListGroupItem>
+            <ListGroupItem>
+              You must be willing to commit at least 4 hours a month for a year. Sessions with your mentee should last 1 hour every other week. We suggest you prep for at least 30 minutes before each of your mentee calls. Lastly, you must allot 1 hour for the monthly virtual chats.
+              We will be hosting a mentor training on X DATE. Attending this training is required if you want to be a part of the program.
+            </ListGroupItem>
+          </ListGroup>
+
+        </Panel>
+
         <Button bsStyle="warning" onClick={() => this.setState({ disclaimer: !this.state.disclaimer })}>
           Toggle Disclaimer
         </Button>
-        <Panel collapsible expanded={this.state.disclaimer}>
-          <p>Here's where the disclaimer goes.</p>
-        </Panel>
 
         <form onSubmit={this.handleFormSubmit}>
-          <label>
-            <p className="asterix">*</p>Username:
-            <input
+          <FormGroup>
+            <ControlLabel>Username *</ControlLabel>
+            <FormControl
+              id="formControlsUsername"
+              type="text"
               name="username"
-              type="text"
-              onChange={this.handleInputChange} />
-          </label>
-          <br/>
-          <label>
-            <p className="asterix">*</p>Password:
-            <input
-              name="password"
+              placeholder=""
+              onChange={this.handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Password *</ControlLabel>
+            <FormControl
+              id="formControlsPassword"
               type="password"
-              onChange={this.handleInputChange} />
-          </label>
-          <br/>
-          <label>
-            <p className="asterix">*</p>First Name:
-            <input
+              name="password"
+              placeholder=""
+              onChange={this.handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>First Name *</ControlLabel>
+            <FormControl
+              id="formControlsFirstName"
+              type="text"
               name="first_name"
+              onChange={this.handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Last Name *</ControlLabel>
+            <FormControl
+              id="formControlsLastName"
               type="text"
-              onChange={this.handleInputChange} />
-          </label>
-          <br/>
-          <label>
-            <p className="asterix">*</p>Last Name:
-            <input
               name="last_name"
-              type="text"
-              onChange={this.handleInputChange} />
-          </label>
-          <br/>
-          <label>
-            <p className="asterix">*</p>Age:
-            <input
-              name="age"
+              onChange={this.handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Age *</ControlLabel>
+            <FormControl
+              id="formControlsAge"
               type="number"
-              value={this.state.age}
-              onChange={this.handleInputChange} />
-          </label>
-          <br/>
-          <label>
-            <p className="asterix">*</p>Email:
-            <input
-              name="email"
+              name="age"
+              onChange={this.handleInputChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Email *</ControlLabel>
+            <FormControl
+              id="formControlsEmail"
               type="email"
-              value={this.state.email}
-              onChange={this.handleInputChange} />
-          </label>
-          <br/>
+              name="email"
+              onChange={this.handleInputChange}
+            />
+          </FormGroup>
           <label>
             <p className="asterix">*</p>City:
             <input
@@ -194,16 +221,22 @@ class MentorApp extends React.Component {
               onChange={this.handleInputChange} />
           </label>
           <br/>
-          <label>
-            <p className="asterix">*</p>How interested are you in giving career advice?
-            <select name="career_advice_rank" value={this.state.career_advice_rank} onChange={this.handleInputChange}>
+          <FormGroup>
+            <ControlLabel>How interested are you in giving career advice? *</ControlLabel>
+            <FormControl
+              componentClass="select"
+              placeholder="select"
+              id="formControlsCareer"
+              name="career_advice_rank"
+              value={this.state.career_advice_rank}
+              onChange={this.handleInputChange}
+            >
               <option value="0">Not at all</option>
               <option value="1">Slightly</option>
               <option value="2">Moderately</option>
               <option value="3">Extremely</option>
-            </select>
-          </label>
-          <br/>
+            </FormControl>
+          </FormGroup>
           <label>
             <p className="asterix">*</p>How interested are you in giving personal advice?
             <select name="personal_advice_rank" value={this.state.personal_advice_rank} onChange={this.handleInputChange}>
@@ -281,7 +314,7 @@ class MentorApp extends React.Component {
               value={this.state.agree_terms}
               onChange={this.handleInputChange} />
           </label>
-          <button type="submit">Apply</button>
+          <Button bsStyle="success" type="submit">Apply</Button>
         </form>
       </div>
     );
