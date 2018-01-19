70a81a6a1b6b41a95ccb4719ec06466632d2248b
Ravi <raviraval@gmail.com>
2018-01-19 12:07:34 -0800

    space around link mentor app confirmation page

--- frontend/components/mentors/appPages/page8.jsx
+++ frontend/components/mentors/appPages/page8.jsx
@@ -15,7 +15,7 @@ class Page8 extends React.Component {
             </h3>
             <h5>You should receive a confirmation email shortly.</h5>
             <p>Feel free to log in
-              <a href="https://girlzftwportal.herokuapp.com/#/">here</a>
+              <a href="https://girlzftwportal.herokuapp.com/#/"> here </a>
                to view and update your application.</p>
           </div>
       )
