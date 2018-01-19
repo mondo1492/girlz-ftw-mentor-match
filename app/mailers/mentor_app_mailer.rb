e2317731153e6d76d4f69a0948cbbb59ed01ef2f
Ravi <raviraval@gmail.com>
2018-01-19 12:06:11 -0800

    mailer works in production!

--- app/mailers/mentor_app_mailer.rb
+++ app/mailers/mentor_app_mailer.rb
@@ -4,6 +4,6 @@ class MentorAppMailer < ApplicationMailer
   def signup_success(user)
     @user = user
     @url = 'http://www.girlzftw.com'
-    mail(to: @user.email, subject: 'Thanks for your application to Girlz,FTW!')
+    mail(to: @user.email, subject: 'Thanks for your application to Girlz, FTW!')
   end
 end
