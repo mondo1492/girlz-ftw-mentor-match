93883b743dcdb90f0233bd21cca2864d673b79ec
Ravi <raviraval@gmail.com>
2018-01-28 20:27:59 -0800

    stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
     # :domain               => 'gmail.com',
     :user_name            => ENV['GMAIL_USERNAME'],
     :password             => ENV['GMAIL_PASSWORD'],
-    :authentication       => "plain",
+    :authentication       => "login",
     :enable_starttls_auto => true
   }
 
